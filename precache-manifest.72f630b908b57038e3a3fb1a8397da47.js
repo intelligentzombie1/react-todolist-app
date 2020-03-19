3d14fbc6a34878c90f3c79032638aa85e898ffe3
intelligentzombie1 <kayodeoshodi400@gmail.com>
2020-03-19 15:21:49 +0100

    Updates

---
+++ precache-manifest.72f630b908b57038e3a3fb1a8397da47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ffc16d9e36c60d59ea91c729989343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2ddaaf3a5bc85d4635c",
+    "url": "/static/css/main.841dc476.chunk.css"
+  },
+  {
+    "revision": "56727218aa3e3fff9586",
+    "url": "/static/js/2.f82b5c25.chunk.js"
+  },
+  {
+    "revision": "3aa8d522e29fa9142f467aacdad35f45",
+    "url": "/static/js/2.f82b5c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ddaaf3a5bc85d4635c",
+    "url": "/static/js/main.239b8463.chunk.js"
+  },
+  {
+    "revision": "a937e5ea1de5d8db1e08",
+    "url": "/static/js/runtime-main.717d4fde.js"
+  }
+]);
